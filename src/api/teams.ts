0be3e553116a42ba11b1941a6be606a966cecb26
KiMDLE <kimdle248@gmail.com>
2024-12-18 12:11:51 +0900

    Update teams.ts

--- src/api/teams.ts
+++ src/api/teams.ts
@@ -13,9 +13,13 @@ export enum ReserveResponse {
 }
 
 export async function postReserves(data: Reserve) {
-  const res = await client.post<Reserve>('/api/reserve', data)
-  if (res.status === 202) return ReserveResponse.OUTLIER;
-  if (res.status === 200) return ReserveResponse.DUPLICATED;
-  if (res.status === 201) return ReserveResponse.CREATED;
-  return ReserveResponse.ERROR;
+  try {
+    const res = await client.post<Reserve>('/api/reserve', data)
+    if (res.status === 202) return ReserveResponse.OUTLIER;
+    if (res.status === 200) return ReserveResponse.DUPLICATED;
+    if (res.status === 201) return ReserveResponse.CREATED;
+    return ReserveResponse.ERROR;
+  } catch {
+    return ReserveResponse.ERROR;
+  }
 }
